828bca56d866b466da69d105939ef1b81563b70d
surahman <surahman.duang@gmail.com>
2019-10-05 08:38:49 +0700

    Add validation max amount to Disbursement

--- Disbursement.php
+++ Disbursement.php
@@ -10,7 +10,7 @@ class Disbursement extends DisplayPrompt
     protected $rules = [
         'bank_code'      => 'string',
         'account_number' => 'numeric',
-        'amount'         => 'numeric|min:10000',
+        'amount'         => 'numeric|min:10000|max:1000000000',
         'remark'         => 'string|max:20'
     ];
 
@@ -19,6 +19,7 @@ class Disbursement extends DisplayPrompt
         'account_number.numeric' => 'Nomor rekening tujuan hanya boleh mengandung angka',
         'amount.numeric'         => 'Nominal harus berupa angka',
         'amount.min'             => 'Minimal penarikan adalah 10000',
+        'amount.max'             => 'Minimal penarikan adalah 1000000000',
         'remark.string'          => 'Karakter yang diperbolehkan hanya berupa huruf',
         'remark.max'             => 'Berita penarikan maksimal 20 karakter',
     ];
